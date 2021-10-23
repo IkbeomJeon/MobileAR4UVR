c94c2df2416ee0475a3781a1b102f558f221f655
IkBeomJeon <jeonikbeom@gmail.com>
2021-10-23 17:09:01 +0900

    add fps contoller

---
+++ Assets/Scripts/IconManager.cs
@@ -0,0 +1,98 @@
+﻿using KCTM.Network.Data;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ARRC_DigitalTwin_Generator;
+using UnityEngine.UI;
+
+public class IconManager : MonoBehaviour
+{
+    public Anchor anchor;
+    Transform cameraTransform;
+    public GameObject[] icon_Prefab = new GameObject[6];
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("UpdateLookat");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    IEnumerator UpdateLookat()
+    {
+        while (true)
+        {
+
+            transform.LookAt(cameraTransform);
+            transform.Rotate(Vector3.up, 180f);
+
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    public void Init(Anchor anchor, string title, string tag, string decription, Transform cameraTransform, float default_height = 0)
+    {
+
+        ///// set position
+        double lon = anchor.point.longitude;
+        double lat = anchor.point.latitude;
+
+            //convert "lon,lat" to "x, y" in unity.
+        Vector3 pos = TerrainUtils.LatLonToWorldWithElevation(TerrainContainer.Instance, lat, lon);
+        transform.localPosition = new Vector3(pos.x, default_height, pos.z);
+
+
+        this.cameraTransform = cameraTransform;
+        this.anchor = anchor;
+
+        
+        //get components
+        var title_text = transform.Find("Canvas/Summary/Title").GetComponent<TMPro.TextMeshProUGUI>();
+        title_text.text = title;
+
+        var tag_image= transform.Find("Canvas/Summary/Tag_Image").GetComponent<Image>();
+        tag_image.color = getTagColor(tag);
+
+        var tag_text = transform.Find("Canvas/Summary/Tag_Image/Tag").GetComponent<TMPro.TextMeshProUGUI>();
+        tag_text.text = tag;
+
+        var desc_text = transform.Find("Canvas/Summary/Description").GetComponent<TMPro.TextMeshProUGUI>();
+        desc_text.text = decription;
+    }
+
+    private Color32 getTagColor(string tag)
+    {
+        Color32 color = new Color32(255, 255, 225, 100);
+        switch (tag)
+        {
+            case "Admission":
+                color = new Color32(64, 224, 208, 100);
+                break;
+            case "Research":
+                color = new Color32(95, 191, 249, 100);
+                break;
+            case "Campus life":
+                color = new Color32(255, 105, 180, 100);
+                break;
+            case "News":
+                color = new Color32(255, 95, 94, 100);
+                break;
+            case "Education":
+            case " Education":
+                color = new Color32(255, 165, 0, 100);
+                break;
+            default: //About
+                color = new Color32(253, 192, 138, 100);
+                break;
+
+        }
+
+        return color;
+    }
+}
---
+++ Assets/Scripts/ResourceLoader.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceLoader : MonoBehaviour
+{
+    //icons
+    public GameObject icon_about;
+    public GameObject icon_admission;
+    public GameObject icon_campusLife;
+    public GameObject icon_education;
+    public GameObject icon_news;
+    public GameObject icon_research;
+
+    //cards
+
+
+    //small cards
+
+    private void Awake()
+    {
+        icon_about = Resources.Load<GameObject>("Prefabs/Icons/Icon_About");
+        icon_admission = Resources.Load<GameObject>("Prefabs/Icons/Icon_Admission");
+        icon_campusLife = Resources.Load<GameObject>("Prefabs/Icons/Icon_CampusLife");
+        icon_education = Resources.Load<GameObject>("Prefabs/Icons/Icon_Education");
+        icon_news = Resources.Load<GameObject>("Prefabs/Icons/Icon_News");
+        icon_research = Resources.Load<GameObject>("Prefabs/Icons/Icon_Research");
+    }
+   
+
+    private static ResourceLoader instance;
+
+    public static ResourceLoader Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                GameObject obj = GameObject.Find("ResourceLoader");
+                if (obj == null)
+                {
+                    obj = new GameObject("ResourceLoader");
+                    instance = obj.AddComponent<ResourceLoader>();
+                }
+                else
+                {
+                    instance = obj.GetComponent<ResourceLoader>();
+                }
+                DontDestroyOnLoad(obj);
+            }
+
+            return instance;
+        }
+    }
+
+}
