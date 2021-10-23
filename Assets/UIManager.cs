f6e27c5b0493fbbfb954dd6b27b739eb77d16896
IkBeomJeon <jeonikbeom@gmail.com>
2021-10-23 16:01:12 +0900

    add map pannel.

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PanelType { search, map };
 public class UIManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject menu;
-    public GameObject searchPannel;
+    //public GameObject menu;
+    //public GameObject searchPanel;
+    //public GameObject mapPanel;
 
-    private void Start()
+    public List<GameObject> panels = new List<GameObject>();
+
+
+    public void Awake()
     {
-        menu = GameObject.Find("UI/Menu");
-        searchPannel = GameObject.Find("UI/SearchPannel");
+        //pannels.Add(GameObject.Find("UI/Menu"));
+        panels.Add(transform.Find("SearchPanel").gameObject);
+        panels.Add(transform.Find("MapPanel").gameObject);
+
+    }
+    public void test()
+    {
+
+    }
+    public void ShowPannel(GameObject panelToShow)
+    {
+        foreach (var panel in panels)
+            panel.SetActive(false);
+        panelToShow.SetActive(true);
     }
 }
